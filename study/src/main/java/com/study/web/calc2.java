4fe24b1856964be355ced76923afc8b844573902
yujongyeop <230233@naver.com>
2022-02-08 14:08:12 +0900

    application 사용

---
+++ study/src/main/java/com/study/web/calc2.java
@@ -0,0 +1,45 @@
+package com.study.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/calc2")
+public class calc2 extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		ServletContext application = request.getServletContext();
+		String v_ = request.getParameter("v");
+		String op = request.getParameter("button");
+		int v = 0;
+		if (v_ != null)
+			v = Integer.parseInt(v_);
+		int result = 0;
+		// 계산
+		if (op.equals("=")) {
+			int x = (Integer) application.getAttribute("value");
+			int y = v;
+			String operator = (String) application.getAttribute("op");
+			if (operator.equals("+")) {
+				result = x + y;
+			} else {
+				result = x - y;
+			}
+			response.getWriter().printf("result is " + result);
+		}
+		// 저장
+		else {
+			application.setAttribute("value", v);
+			application.setAttribute("op", op);
+		}
+	}
+}
